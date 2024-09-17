3eed195295422e7ebcf96975b572bc58d205bb6b
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-09-17 16:04:35 +0300

    Completed translating from ORMS to SQLAlchemy.

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,48 @@
-from models import Dog
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine = None):
+    if engine is None:
+        engine = create_engine("sqlite:///:memory:")
+    base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    return session
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return [dog for dog in session.query(Dog).all()]
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+
+#function calls
+if __name__ == "__main__":
+    session = create_table(Base)
+
+    dog1 = Dog(
+        name = "Joey",
+        breed = "Labrador Retriever"
+    )
+
+    save(session, dog1)
+    find_by_name(session, "Joey")
+    find_by_id(session, 1)
+    find_by_name_and_breed(session, "Milan", "Beagle")
+    update_breed(session, dog1, "Beagle")
